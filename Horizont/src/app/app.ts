785d366ae5d595683d788eb296634f90aa7ecd86
ST <eszesztiti@gmail.com>
2026-06-28 15:34:53 +0200

    Add categories and search to header

--- Horizont/src/app/app.ts
+++ Horizont/src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Search } from './components/search/search';
+import { Categories } from './components/categories/categories';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     Header,
-    Search
+    Search,
+    Categories
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- Horizont/src/app/components/header/header.ts
+++ Horizont/src/app/components/header/header.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { faBookOpen, faHeadphones, faVideoCamera } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Search } from '../search/search';
+import { Categories } from '../categories/categories';
 
 @Component({
   selector: 'myHeader',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,
+    Categories,
+    Search
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
